c93faaa1d7bff62bafb52c79c8e6ee6426693f00
marthel <marthel@kth.se>
2016-12-15 14:23:15 +0100

    asdaw

--- src/main/java/vertx/ChatVerticle.java
+++ src/main/java/vertx/ChatVerticle.java
@@ -4,6 +4,9 @@ import io.vertx.core.AbstractVerticle;
 import io.vertx.core.buffer.Buffer;
 import io.vertx.core.eventbus.EventBus;
 import io.vertx.core.eventbus.MessageConsumer;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.http.ServerWebSocket;
+import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.StaticHandler;
 import io.vertx.ext.web.handler.sockjs.*;
@@ -11,6 +14,8 @@ import io.vertx.ext.web.handler.sockjs.*;
 import java.text.DateFormat;
 import java.time.Instant;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * Created by Marthin on 2016-12-14.
@@ -18,30 +23,42 @@ import java.util.Date;
  * https://github.com/vert-x3/vertx-examples/tree/master/web-examples/src/main/java/io/vertx/example/web/chat
  */
 public class ChatVerticle extends AbstractVerticle{
-    SockJSSocket hej;
+    private HashMap<String,<Integer,ServerWebSocket>> socketHashMap;
+
+    public ChatVerticle() {
+        socketHashMap = new HashMap<>();
+    }
+
+    @Override
+    public void start() {
+        HttpServer server = vertx.createHttpServer().websocketHandler(serverWebSocket -> {
+            System.out.println("Connected!" + serverWebSocket.toString());
+            serverWebSocket.closeHandler(close -> {
+                socketHashMap.remove(vertx.sharedData().)
+                System.out.println("closed" + socketHashMap.size());
+
+            });
+            serverWebSocket.frameHandler(webSocketFrame -> {
+                System.out.println(webSocketFrame.textData());
+                serverWebSocket.write(Buffer.buffer("hello"));
+                //socketHashMap.put(serverWebSocket.textHandlerID(),serverWebSocket);
+                System.out.println(socketHashMap.size());
+            });
+        }).requestHandler(req -> {
+            req.response().sendFile("webroot/index.html");
+        }).listen(8080);
+    }
     @Override
     public void start() {
         Router router = Router.router(vertx);
-
-        // Allow events for the designated addresses in/out of the event bus bridge
         BridgeOptions options = new BridgeOptions()
-                .addInboundPermitted(new PermittedOptions().setAddress("chat-server"));
-
-        // Create the event bus bridge and add it to the router.
+                .addInboundPermitted(new PermittedOptions().setAddress("chat.server"))
+                .addOutboundPermitted(new PermittedOptions().setAddressRegex("chat\\.[0-9]+"));
         router.route("/eventbus/*").handler(SockJSHandler.create(vertx).bridge(options, event -> {
-
-            // You can also optionally provide a handler like this which will be passed any events that occur on the bridge
-            // You can use this for monitoring or logging, or to change the raw messages in-flight.
-            // It can also be used for fine grained access control.
-
             if (event.type() == BridgeEventType.SOCKET_CREATED) {
                 System.out.println("A socket was created");
-                hej = event.socket();
             }
-
-            // This signals that it's ok to process the event
             event.complete(true);
-
         }));
 
         // Create a router endpoint for the static content.
@@ -52,11 +69,12 @@ public class ChatVerticle extends AbstractVerticle{
 
         EventBus eb = vertx.eventBus();
         // Register to listen for messages coming IN to the server
-        MessageConsumer<Object> handler = eb.consumer("chat-server").handler(message -> {
+        eb.consumer("chat-server").handler(message -> {
             // Create a timestamp string
             String timestamp = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.MEDIUM).format(Date.from(Instant.now()));
             // Send the message back out to all clients with the timestamp prepended.
-            eb.publish("chat-client", timestamp + ": " + message.body());
+            eb.publish("chat.34", timestamp + ": " + message.body());
         });
     }
+
 }
